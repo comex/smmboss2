3129f45ff3b2b7f038c8a02cbba2aefe792ba817
comex <comexk@gmail.com>
2024-05-31 19:34:02 -0700

    kill the namespace

--- source/program/main.cpp
+++ source/program/main.cpp
@@ -38,9 +38,6 @@ static uintptr_t assert_nonzero(uintptr_t offset) {
     return offset;
 }
 
-// TODO: I decided I hate this namespace
-namespace mm {
-
 // -- Naming
 //
 // All of these classes are things defined either in Mario Maker or in the SDK.
@@ -73,7 +70,7 @@ namespace mm {
 // - Making it easier to have one binary that supports multiple versions of
 //   Mario Maker (which might have fields at different offsets).
 
-union pointer_to_member_function {
+union mm_pointer_to_member_function {
     struct {
         uint64_t _;
         uint64_t is_virt:1,
@@ -98,78 +95,78 @@ union pointer_to_member_function {
         }
     }
 };
-static_assert(sizeof(pointer_to_member_function) == 0x10);
+static_assert(sizeof(mm_pointer_to_member_function) == 0x10);
 
 // sead::ListNode
-struct ListNode {
-    ListNode *prev;
-    ListNode *next;
+struct mm_ListNode {
+    mm_ListNode *prev;
+    mm_ListNode *next;
 };
 
 // sead::ListImpl
-struct ListImpl {
-    ListNode head;
+struct mm_ListImpl {
+    mm_ListNode head;
     uint32_t count;
     uint32_t offset_to_link;
 };
 
 // wrapper, no evidence for original
 template <typename T>
-struct list_iterator {
-    ListNode *cur_node_;
+struct mm_list_iterator {
+    mm_ListNode *cur_node_;
     uint32_t offset_to_link_;
-    ListNode *end_;
+    mm_ListNode *end_;
 
     T &operator*() const {
         assert(cur_node_ != end_);
         return *(T *)((char *)cur_node_ - offset_to_link_);
     }
-    list_iterator &operator++() {
+    mm_list_iterator &operator++() {
         assert(cur_node_ != end_);
         cur_node_ = cur_node_->next;
         return *this;
     }
-    bool operator==(const list_iterator &other) const {
+    bool operator==(const mm_list_iterator &other) const {
         return cur_node_ == other.cur_node_;
     };
 };
 
 // wrapper, no evidence for original
 template <typename T>
-struct list : public ListImpl {
-    list_iterator<T> begin() { return list_iterator<T>{head.next, offset_to_link, &head}; }
-    list_iterator<T> end()   { return list_iterator<T>{&head, offset_to_link, &head}; }
+struct mm_list : public mm_ListImpl {
+    mm_list_iterator<T> begin() { return mm_list_iterator<T>{head.next, offset_to_link, &head}; }
+    mm_list_iterator<T> end()   { return mm_list_iterator<T>{&head, offset_to_link, &head}; }
 };
 
 template <typename T>
-struct count_ptr {
+struct mm_count_ptr {
     uint32_t count;
     pt_pointer<T> ptr;
 };
 
 template <typename T>
-struct count_cap_ptr {
+struct mm_count_cap_ptr {
     uint32_t count;
     uint32_t cap;
     pt_pointer<T> ptr;
 };
 
-struct SafeString {
+struct mm_SafeString {
     PROP(vtable,     0x0, void *);
     PROP(str,        0x8, const char *);
     PSEUDO_TYPE_SIZE(0x10);
 };
 
 // Lp::Utl::StateMachine::Delegate<T>
-struct StateMachineDelegate {
+struct mm_StateMachineDelegate {
     PROP(vtable,     0x0,  void *);
     PROP(owner,      0x8,  void *);
-    PROP(enter,      0x10, pointer_to_member_function);
-    PROP(exec,       0x20, pointer_to_member_function);
-    PROP(exit,       0x30, pointer_to_member_function);
+    PROP(enter,      0x10, mm_pointer_to_member_function);
+    PROP(exec,       0x20, mm_pointer_to_member_function);
+    PROP(exit,       0x30, mm_pointer_to_member_function);
     PSEUDO_TYPE_SIZE(0x40);
 
-    pointer_to_member_function &callback_n(size_t n) {
+    mm_pointer_to_member_function &callback_n(size_t n) {
         switch (n) {
             case 0: return enter();
             case 1: return exec();
@@ -180,14 +177,14 @@ struct StateMachineDelegate {
 };
 
 // Lp::Utl::StateMachine
-struct StateMachine {
+struct mm_StateMachine {
     PROP(state,   0x8, uint32_t);
-    PROP(states, 0x28, count_ptr<StateMachineDelegate>);
-    PROP(names,  0x38, count_ptr<SafeString>);
+    PROP(states, 0x28, mm_count_ptr<mm_StateMachineDelegate>);
+    PROP(names,  0x38, mm_count_ptr<mm_SafeString>);
     PSEUDO_TYPE_SIZE(0x48);
 };
 
-struct hitbox {
+struct mm_hitbox {
     PROP(vtable, 0x0, uintptr_t);
     PSEUDO_TYPE_SIZE(0x1c8);
 
@@ -201,41 +198,41 @@ struct hitbox {
     };
 };
 
-struct hitbox_node {
-    PROP(node,     0, ListNode);
-    PROP(owner, 0x10, hitbox *);
+struct mm_hitbox_node {
+    PROP(node,     0, mm_ListNode);
+    PROP(owner, 0x10, mm_hitbox *);
     PROP(list,  0x18, void *);
     PSEUDO_TYPE_UNSIZED;
 };
 
-struct hitbox_manager {
-    PROP(lists,  0x10, list<hitbox_node>[5]);
+struct mm_hitbox_manager {
+    PROP(lists,  0x10, mm_list<mm_hitbox_node>[5]);
     PSEUDO_TYPE_UNSIZED;
 };
 
-struct terrain_manager {
+struct mm_terrain_manager {
 
     PSEUDO_TYPE_UNSIZED;
 };
 
-struct normal_collider;
-struct scol_collider;
+struct mm_normal_collider;
+struct mm_scol_collider;
 
 // This is really one of two unrelated structs, and the real way to distinguish
 // them is via the vtable on the node, but distinguishing them by their own
 // vtables is just a bit easier...
-struct some_collider {
+struct mm_some_collider {
     PROP(vtable, 0, void *);
     PSEUDO_TYPE_UNSIZED;
 
-    std::variant<normal_collider *, scol_collider *, std::monostate> downcast();
+    std::variant<mm_normal_collider *, mm_scol_collider *, std::monostate> downcast();
     uintptr_t vt20_offset() const {
         uintptr_t vt20 = *(uintptr_t *)((char *)vtable() + 0x20);
         return vt20 - exl::util::modules::GetTargetStart();
     }
 };
 
-struct normal_collider : public some_collider {
+struct mm_normal_collider : public mm_some_collider {
     // virtual method @ 0x20 for all subclasses:
     // (we don't care about the specific subclass for now)
     static constexpr uintptr_t vt20_offset[VER_COUNT] = {
@@ -244,7 +241,7 @@ struct normal_collider : public some_collider {
 
 };
 
-struct scol_collider : public some_collider {
+struct mm_scol_collider : public mm_some_collider {
     // virtual method @ 0x20 for two subclasses:
     // (we don't care about the specific subclass for now)
     static constexpr uintptr_t vt20_offset_1[VER_COUNT] = {
@@ -258,14 +255,14 @@ struct scol_collider : public some_collider {
 };
 
 // Figure out what type of object this is.
-std::variant<normal_collider *, scol_collider *, std::monostate>
-some_collider::downcast() {
+std::variant<mm_normal_collider *, mm_scol_collider *, std::monostate>
+mm_some_collider::downcast() {
     uintptr_t vt20_offset = this->vt20_offset();
-    if (vt20_offset == assert_nonzero(normal_collider::vt20_offset[s_cur_mm_version])) {
-        return (normal_collider *)this;
-    } else if (vt20_offset == assert_nonzero(scol_collider::vt20_offset_1[s_cur_mm_version]) ||
-               vt20_offset == assert_nonzero(scol_collider::vt20_offset_2[s_cur_mm_version])) {
-        return (scol_collider *)this;
+    if (vt20_offset == assert_nonzero(mm_normal_collider::vt20_offset[s_cur_mm_version])) {
+        return (mm_normal_collider *)this;
+    } else if (vt20_offset == assert_nonzero(mm_scol_collider::vt20_offset_1[s_cur_mm_version]) ||
+               vt20_offset == assert_nonzero(mm_scol_collider::vt20_offset_2[s_cur_mm_version])) {
+        return (mm_scol_collider *)this;
     } else {
         // unknown type
         return std::monostate{};
@@ -274,38 +271,36 @@ some_collider::downcast() {
 
 // TODO: cleanup naming for all collider stuff :(
 
-struct some_collider_node;
+struct mm_some_collider_node;
 
-struct some_collider_node_outer {
-    PROP(node,  0x20, some_collider_node);
-    PROP(owner, 0x48, some_collider *);
+struct mm_some_collider_node_outer {
+    PROP(node,  0x20, mm_some_collider_node);
+    PROP(owner, 0x48, mm_some_collider *);
     PSEUDO_TYPE_UNSIZED;
 };
 
-struct some_collider_node {
-    PROP(list_node, 0x0,  ListNode);
-    PROP(outer,     0x10, some_collider_node_outer *);
+struct mm_some_collider_node {
+    PROP(list_node, 0x0,  mm_ListNode);
+    PROP(outer,     0x10, mm_some_collider_node_outer *);
     PSEUDO_TYPE_UNSIZED;
 };
 
-struct BgCollisionSystem {
-    PROP(colliders1, 0x38, list<mm::some_collider_node>);
-    PROP(colliders2, 0x58, list<mm::some_collider_node>);
+struct mm_BgCollisionSystem {
+    PROP(colliders1, 0x38, mm_list<mm_some_collider_node>);
+    PROP(colliders2, 0x58, mm_list<mm_some_collider_node>);
 
     PSEUDO_TYPE_UNSIZED;
 };
 
-struct AreaSystem {
+struct mm_AreaSystem {
     PROP(world_id,            0x18, uint32_t);
-    PROP(hitbox_mgr,          0x40, hitbox_manager *);
-    PROP(bg_collision_system, 0x90, BgCollisionSystem *);
-    PROP(terrain_mgr,         0xa0, terrain_manager *);
+    PROP(hitbox_mgr,          0x40, mm_hitbox_manager *);
+    PROP(bg_collision_system, 0x90, mm_BgCollisionSystem *);
+    PROP(terrain_mgr,         0xa0, mm_terrain_manager *);
     PSEUDO_TYPE_UNSIZED;
 };
 
-} // namespace mm
-
-static const char *get_state_name(mm::StateMachine *sm, uint32_t state) {
+static const char *get_state_name(mm_StateMachine *sm, uint32_t state) {
     if (state < sm->names().count) {
         return sm->names().ptr[state].str();
     } else {
@@ -314,10 +309,10 @@ static const char *get_state_name(mm::StateMachine *sm, uint32_t state) {
 }
 
 __attribute__((noinline))
-static uintptr_t get_state_callback(mm::StateMachine *sm, uint32_t state, uint32_t which_callback) {
+static uintptr_t get_state_callback(mm_StateMachine *sm, uint32_t state, uint32_t which_callback) {
     EXL_ASSERT(state < sm->states().count);
-    mm::StateMachineDelegate *delegate = &sm->states().ptr[state];
-    mm::pointer_to_member_function cb = delegate->callback_n(which_callback);
+    mm_StateMachineDelegate *delegate = &sm->states().ptr[state];
+    mm_pointer_to_member_function cb = delegate->callback_n(which_callback);
     uintptr_t func = (uintptr_t)cb.resolve(delegate->owner());
     if (func) {
         func -= exl::util::modules::GetTargetStart(); // unslide address
@@ -340,7 +335,7 @@ static void *return_address_from_frame_impl(void *frame0, size_t n) {
     return_address_from_frame_impl(__builtin_frame_address(0), n)
 
 HOOK_DEFINE_TRAMPOLINE(Stub_StateMachine_changeState) {
-    static void Callback(mm::StateMachine *sm, int state) {
+    static void Callback(mm_StateMachine *sm, int state) {
         int old_state = sm->state();
         xprintf("%p.set_state(%s(%d) -> %s(%d) in:0x%lx tick:0x%lx out:0x%lx (obj:%p)) <- %p <- %p <- %p <- %p <- %p",
             sm,
@@ -440,7 +435,7 @@ struct SeenCache {
 
 SeenCache s_seen_cache;
 
-static void report_hitbox(mm::hitbox *hb, bool surprise) {
+static void report_hitbox(mm_hitbox *hb, bool surprise) {
     hb->verify();
     if (!s_seen_cache.test_and_set(hb)) {
         if (surprise) {
@@ -455,7 +450,7 @@ static void report_hitbox(mm::hitbox *hb, bool surprise) {
 }
 
 HOOK_DEFINE_TRAMPOLINE(Stub_hitbox_collide) {
-    static long Callback(mm::hitbox *hb1, mm::hitbox *hb2) {
+    static long Callback(mm_hitbox *hb1, mm_hitbox *hb2) {
         long ret = Orig(hb1, hb2);
         report_hitbox(hb1, /*surprise*/ true);
         report_hitbox(hb2, /*surprise*/ true);
@@ -472,26 +467,26 @@ HOOK_DEFINE_TRAMPOLINE(Stub_hitbox_collide) {
     };
 };
 
-static void report_all_hitboxes(mm::AreaSystem *as) {
-    for (mm::list<mm::hitbox_node> &list : as->hitbox_mgr()->lists()) {
-        for (mm::hitbox_node &hn : list) {
+static void report_all_hitboxes(mm_AreaSystem *as) {
+    for (mm_list<mm_hitbox_node> &list : as->hitbox_mgr()->lists()) {
+        for (mm_hitbox_node &hn : list) {
             report_hitbox(hn.owner(), /*surprise*/ false);
         }
     }
 }
 
-static void report_all_colliders_in(mm::list<mm::some_collider_node> *cnlist, int which_list) {
-    for (mm::some_collider_node &cn : *cnlist) {
-        mm::some_collider *some = cn.outer()->owner();
+static void report_all_colliders_in(mm_list<mm_some_collider_node> *cnlist, int which_list) {
+    for (mm_some_collider_node &cn : *cnlist) {
+        mm_some_collider *some = cn.outer()->owner();
         auto downcasted = some->downcast();
-        if (auto ncp = std::get_if<mm::normal_collider *>(&downcasted)) {
-            mm::normal_collider *nc = *ncp;
+        if (auto ncp = std::get_if<mm_normal_collider *>(&downcasted)) {
+            mm_normal_collider *nc = *ncp;
             s_hose.write_fixed(
                 hose_tag{"normcol"},
                 nc
             );
-        } else if (auto scp = std::get_if<mm::scol_collider *>(&downcasted)) {
-            mm::scol_collider *sc = *scp;
+        } else if (auto scp = std::get_if<mm_scol_collider *>(&downcasted)) {
+            mm_scol_collider *sc = *scp;
             s_hose.write_fixed(
                 hose_tag{"scolcol"},
                 sc
@@ -505,13 +500,13 @@ static void report_all_colliders_in(mm::list<mm::some_collider_node> *cnlist, in
     }
 }
 
-static void report_all_colliders(mm::AreaSystem *as) {
+static void report_all_colliders(mm_AreaSystem *as) {
     report_all_colliders_in(&as->bg_collision_system()->colliders1(), 1);
     report_all_colliders_in(&as->bg_collision_system()->colliders2(), 2);
 }
 
 HOOK_DEFINE_TRAMPOLINE(Stub_AreaSystem_do_many_collisions) {
-    static void Callback(mm::AreaSystem *self) {
+    static void Callback(mm_AreaSystem *self) {
         s_seen_cache.next_frame();
         s_hose.write_fixed(
             hose_tag{"do_many"},

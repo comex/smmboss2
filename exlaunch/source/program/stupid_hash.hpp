1b60d3effb866c987b82a7ab8fa750bf49c0d893
comex <comexk@gmail.com>
2024-06-03 11:22:40 -0700

    wow omg it's a hash table

--- exlaunch/source/program/stupid_hash.hpp
+++ exlaunch/source/program/stupid_hash.hpp
@@ -1,6 +1,15 @@
+#ifndef FUZZ_TEST
+#pragma once
+#endif
+
 #include <stdint.h>
 #include <stddef.h>
 #include <utility>
+#ifdef FUZZ_TEST
+#include <assert.h>
+#else
+#include "stuff.h"
+#endif
 
 template <typename T>
 struct __attribute__((packed)) Packed {
@@ -14,12 +23,10 @@ struct __attribute__((packed)) Packed {
     }
 };
 
-#pragma once
 template <typename KeyTy, typename ValTy, size_t NUM_HASH_SLOTS, size_t NUM_ENTRY_SLOTS>
 struct StupidHash {
     using EntryIdx = uint16_t;
     static_assert(NUM_ENTRY_SLOTS < (1 << (8 * sizeof(EntryIdx))));
-    static constexpr EntryIdx SENTINEL_ENTRY_IDX = (EntryIdx)-1;
 
     struct Entry {
         Packed<EntryIdx> prev, next;
@@ -27,39 +34,39 @@ struct StupidHash {
         ValTy value;
     };
 
-    EntryIdx *prevs_next(Entry *entry) {
-        if (entry->prev == SENTINEL_ENTRY_IDX) {
+    Packed<EntryIdx> *prevs_next(Entry *entry) {
+        if (entry->prev == 0) {
             return &by_hash_[hash(entry->key)];
         } else {
-            return &entries_[entry->prev].next;
+            return &entry_at_idx(entry->prev)->next;
         }
     }
 
-    EntryIdx *nexts_prev(Entry *entry) {
-        if (entry->next == SENTINEL_ENTRY_IDX) {
+    Packed<EntryIdx> * nexts_prev(Entry *entry) {
+        if (entry->next == 0) {
             return nullptr;
         } else {
-            return &entries_[entry->prev].prev;
+            return &entry_at_idx(entry->next)->prev;
         }
     }
 
     void move_entry(EntryIdx old_idx, EntryIdx new_idx) {
-        Entry *old_entry = &entries_[old_idx];
-        Entry *new_entry = &entries_[new_idx];
+        Entry *old_entry = entry_at_idx(old_idx);
+        Entry *new_entry = entry_at_idx(new_idx);
         *new_entry = *old_entry;
 
         *prevs_next(old_entry) = new_idx;
-        if (EntryIdx *p = nexts_prev(old_entry)) {
+        if (Packed<EntryIdx> *p = nexts_prev(old_entry)) {
             *p = new_idx;
         }
     }
 
     void unlink_entry(EntryIdx entry_idx) {
-        Entry *entry = &entries_[entry_idx];
+        Entry *entry = entry_at_idx(entry_idx);
         EntryIdx prev = entry->prev, next = entry->next;
 
         *prevs_next(entry) = next;
-        if (EntryIdx *p = nexts_prev(entry)) {
+        if (Packed<EntryIdx> *p = nexts_prev(entry)) {
             *p = prev;
         }
     }
@@ -68,7 +75,7 @@ struct StupidHash {
         EntryIdx entry_idx = entry_to_idx(entry);
         unlink_entry(entry_idx);
         assert(entries_count_ > 0);
-        EntryIdx victim_idx = (EntryIdx)--entries_count_;
+        EntryIdx victim_idx = (EntryIdx)entries_count_--;
         if (victim_idx != entry_idx) {
             move_entry(victim_idx, entry_idx);
         }
@@ -77,15 +84,20 @@ struct StupidHash {
     EntryIdx entry_to_idx(Entry *entry) {
         size_t offset = (uintptr_t)entry - (uintptr_t)entries_;
         assert(offset < sizeof(entries_) && offset % sizeof(Entry) == 0);
-        return offset / sizeof(Entry);
+        return offset / sizeof(Entry) + 1;
+    }
+
+    Entry *entry_at_idx(EntryIdx idx) {
+        assert(idx > 0 && idx <= NUM_ENTRY_SLOTS);
+        return &entries_[idx - 1];
     }
 
     std::pair<Entry *, bool> find_entry(KeyTy key, bool insert) {
-        EntryIdx *next_p = &by_hash_[hash(key)], idx = *next_p;
-        EntryIdx expected_prev = SENTINEL_ENTRY_IDX;
+        Packed<EntryIdx> *next_p = &by_hash_[hash(key)], idx = *next_p;
+        EntryIdx expected_prev = 0;
         Entry *entry;
-        while (idx != SENTINEL_ENTRY_IDX) {
-            entry = &entries_[idx];
+        while (idx != 0) {
+            entry = entry_at_idx(idx);
             assert(entry->prev == expected_prev);
             if (entry->key == key) {
                 return std::make_pair(entry, true);
@@ -97,9 +109,9 @@ struct StupidHash {
         if (insert) {
             EntryIdx new_idx = alloc_entry_idx();
             *next_p = new_idx;
-            entry = &entries_[new_idx];
+            entry = entry_at_idx(new_idx);
             entry->prev = expected_prev;
-            entry->next = SENTINEL_ENTRY_IDX;
+            entry->next = 0;
             entry->key = key;
         } else {
             entry = nullptr;
@@ -109,12 +121,121 @@ struct StupidHash {
 
     EntryIdx alloc_entry_idx() {
         if (entries_count_ == NUM_ENTRY_SLOTS) {
+#ifdef FUZZ_TEST
+            assert(false);
+#else
             panic("%s: hash table is full", __PRETTY_FUNCTION__);
+#endif
         }
-        return (EntryIdx)entries_count_++;
+        return (EntryIdx)++entries_count_;
     }
 
-    Entry entries_[NUM_ENTRY_SLOTS];
-    size_t entries_count_;
-    EntryIdx by_hash_[NUM_HASH_SLOTS];
+    size_t hash(const KeyTy &key) {
+        return (size_t)key % NUM_HASH_SLOTS;
+    }
+
+    Entry *begin() { return entries_; }
+    Entry *end() { return entries_ + entries_count_; }
+
+    size_t size() const { return entries_count_; }
+    size_t capacity() const { return NUM_ENTRY_SLOTS; }
+
+    Entry entries_[NUM_ENTRY_SLOTS]{};
+    size_t entries_count_ = 0;
+    Packed<EntryIdx> by_hash_[NUM_HASH_SLOTS]{};
 };
+
+#ifdef FUZZ_TEST
+/*
+clang++ -std=c++20 -o /tmp/sh-fuzz -fsanitize=fuzzer -DFUZZ_TEST -x c++ stupid_hash.hpp -O2 -g3
+*/
+#include <unordered_map>
+#include <span>
+#include <vector>
+namespace fuzz {
+    using KeyTy = uint32_t;
+    using ValTy = uint32_t;
+    using SH = StupidHash<KeyTy, ValTy, 3000, 3000>;
+    using UM = std::unordered_map<KeyTy, ValTy>;
+
+    template <typename T>
+    T shift(std::span<const uint8_t> *span) {
+        T t{};
+        size_t size = std::min(span->size(), sizeof(T));
+        memcpy(&t, span->data(), size);
+        *span = span->subspan(size);
+        return t;
+    }
+
+    static void verify(SH &sh, const UM &um) {
+        std::vector<std::pair<KeyTy, ValTy>> sh_kvs, um_kvs;
+        for (SH::Entry &entry : sh) {
+            sh_kvs.emplace_back(entry.key, entry.value);
+        }
+        for (const auto &pair : um) {
+            um_kvs.emplace_back(pair);
+        }
+        std::sort(sh_kvs.begin(), sh_kvs.end());
+        std::sort(um_kvs.begin(), um_kvs.end());
+        assert(sh_kvs == um_kvs);
+    }
+
+    extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+        std::span<const uint8_t> span(data, size);
+        SH sh;
+        UM um;
+        for (size_t i = 0; i < sizeof(sh); i++) {
+            assert(((char *)&sh)[i] == 0);
+        }
+        while (!span.empty()) {
+            assert(sh.entries_count_ == um.size());
+            switch (shift<int>(&span) % 2) {
+            case 0: {
+                auto key = shift<KeyTy>(&span);
+                auto val = shift<ValTy>(&span);
+                if (sh.size() == sh.capacity()) {
+                    break;
+                }
+                auto it = um.find(key);
+                bool insert = shift<uint8_t>(&span);
+                auto [entry, was_found] = sh.find_entry(key, insert);
+                if (was_found) {
+                    assert(it != um.end());
+                    assert(entry->key == key);
+                    assert(it->second == entry->value);
+                    it->second = val;
+                    entry->value = val;
+                } else {
+                    assert(it == um.end());
+                    if (insert) {
+                        assert(entry);
+                        assert(entry->key == key);
+                        entry->value = val;
+                        um[key] = val;
+                    } else {
+                        assert(!entry);
+                    }
+                }
+                break;
+            }
+            case 1:  {
+                if (sh.size() == 0) {
+                    break;
+                }
+                KeyTy key = sh.entries_[shift<uint32_t>(&span) % sh.size()].key;
+                auto [entry, was_found] = sh.find_entry(key, false);
+                assert(was_found);
+                sh.remove_entry(entry);
+                auto [entry2, was_found2] = sh.find_entry(key, false);
+                assert(!was_found2);
+                assert(um.erase(key) == 1);
+            }
+            }
+
+        }
+        verify(sh, um);
+        return 0;
+    }
+
+} // namespace fuzz
+#endif
